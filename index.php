0efee7cfb7dfc6cb9cbe2d8632ef821cbd207cc6
FrancoMongi <franmongi8@gmail.com>
2023-06-22 14:56:18 -0300

    hola

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola Mcqueen</title>
+</head>
+<body>
+    <h1>Quien es tu auto favorito</h1>
+    <h3>Mcqueen</h3>
+</body>
+</html>
